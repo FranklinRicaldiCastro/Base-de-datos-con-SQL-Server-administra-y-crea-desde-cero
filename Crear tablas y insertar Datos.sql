e7fd9df9e9a84fed17ad5b83a5617b2d61e815ca
RICALDI CASTRO Franklin Antolin <fricaldic@gmail.com>
2022-03-31 10:04:21 -0500

    Mi primer commit3

--- Crear tablas y insertar Datos.sql
+++ Crear tablas y insertar Datos.sql
@@ -4,21 +4,21 @@ CREATE TABLE libros(
 	cantidad INT,
 	disponibles INT
 );
-INSERT INTO libros(idLibros, nombre	, cantidad, disponibles) VALUES('1-B', 'El Alquimista', 10, 8);
+--INSERT INTO libros(idLibros, nombre	, cantidad, disponibles) VALUES('1-B', 'El Alquimista', 10, 8);
 
-CREATE TABLE clientes(
-	idcliente VARCHAR(10) PRIMARY KEY,
-	nombre VARCHAR(20),
-	edad INT,
-	direccion VARCHAR(100),
-	idLibros VARCHAR(10) FOREIGN KEY REFERENCES libros(idLibros)
-);
+--CREATE TABLE clientes(
+--	idcliente VARCHAR(10) PRIMARY KEY,
+--	nombre VARCHAR(20),
+--	edad INT,
+--	direccion VARCHAR(100),
+--	idLibros VARCHAR(10) FOREIGN KEY REFERENCES libros(idLibros)
+--);
 
-INSERT INTO clientes(idcliente, nombre, edad, direccion, idLibros) VALUES('2-A', 'Thanos Snachez', 50, 'calle k', '1-B')
+--INSERT INTO clientes(idcliente, nombre, edad, direccion, idLibros) VALUES('2-A', 'Thanos Snachez', 50, 'calle k', '1-B')
 
 
 --SELECT*FROM libros
 --SELECT*FROM clientes
 
 --CLIENTES QUE TIENEN EL LIBRO '1-B'
-SELECT*FROM clientes WHERE idLibros = '1-B'
+--SELECT*FROM clientes WHERE idLibros = '1-B'
